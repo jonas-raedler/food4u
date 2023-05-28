f918537322e09599d63ecf36594384bb63c9281e
jonas-raedler <jraedler@bu.edu>
2023-05-28 12:58:19 +0200

    added ingredient adding functionality

--- food4u/recipes/static/_js_functionality.js
+++ food4u/recipes/static/_js_functionality.js
@@ -1,22 +1,25 @@
 // Functionality for Adding & Removing Input Fields
 
-    $('.add').on('click', add);
-    $('.remove').on('click', remove);
-
 function add() {
-  var new_ingredient_no = parseInt($('#total_number').val()) + 1;
-  var new_input = "<label for='ingredient" + new_ingredient_no + "'>Ingredient:</label><input type='text' id='ingredient" + new_ingredient_no + "' name='ingredient" + new_ingredient_no + "'>";
+    var new_ingredient_no = parseInt($('#total_number').val()) + 1;
+    var new_input = "" +
+        "<div id=ingr_form_item_" + new_ingredient_no + ">" +
+            "<label for='ingredient_" + new_ingredient_no + "'>Ingredient:&nbsp</label>" +
+            "<input type='text' id='ingredient_" + new_ingredient_no + "' name='ingredient_" + new_ingredient_no + "'>" +
+        "</div>";
 
-  $('#ingredient_list').append(new_input);
-  $('#total_number').val(new_chq_no);
-  console.log(3);
+    $('#ingredient_list').append(new_input);
+    $('#total_number').val(new_ingredient_no);
+
+    document.getElementById("test").innerHTML = "hahaha";
+    console.log(new_ingredient_no);
 }
 
 function remove() {
-  var last_ingredient_no = $('#total_number').val();
+    var last_ingredient_no = $('#total_number').val();
 
-  if (last_ingredient_no > 1) {
-    $('#ingredient_' + last_chq_no).remove();
-    $('#total_number').val(last_chq_no - 1);
-  }
+    if (last_ingredient_no > 1) {
+        $('#ingr_form_item_' + last_ingredient_no).remove();
+        $('#total_number').val(last_ingredient_no - 1);
+    }
 }
